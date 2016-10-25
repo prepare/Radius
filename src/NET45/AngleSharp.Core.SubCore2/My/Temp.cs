3abe6dc24a3c68a72acfb22d073b94f7bb9e81ba
win <wintercoredev@gmail.com>
2016-10-25 09:25:57 +0700

    05: It's called Angle not Angular, my bad! :)

---
+++ src/NET45/AngleSharp.Core.SubCore2/My/Temp.cs
@@ -0,0 +1,22 @@
+﻿
+using AngleSharp.Dom.Css;
+namespace AngularSharp
+{
+    public interface CssMedium : CssNode, ICssMedium
+    {
+    }
+    public interface CssNode : ICssNode
+    {
+    }
+    public interface CssStyleDeclaration
+    {
+    }
+    public interface CssValue
+    {
+    }
+    public interface CssRule : ICssRule { }
+
+    public interface HtmlDocument { }
+
+    
+}
---
+++ src/NET45/AngleSharp.Core.SubCoreCss/My/StringExtensions1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AngleSharp.Extensions
+{
+    static class CssStringExtensions
+    {
+        /// <summary>
+        /// Creates a CSS function from the string with the given argument.
+        /// </summary>
+        /// <param name="value">The CSS function name.</param>
+        /// <param name="argument">The CSS function argument.</param>
+        /// <returns>The CSS function string.</returns>
+        public static String CssFunction(this String value, String argument)
+        {
+            return String.Concat(value, "(", argument, ")");
+        }
+    }
+}
